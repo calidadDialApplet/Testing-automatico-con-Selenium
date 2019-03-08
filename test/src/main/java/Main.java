8cd3befebf48c9ace2f3278d650fc9b514307c4f
dflor <david.flor@dialapplet.com>
2019-03-08 18:55:04 +0100

    Simple interface added --- Se añadió interfaz simple

--- test/src/main/java/Main.java
+++ test/src/main/java/Main.java
@@ -1,8 +1,31 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+        Button button;
 
         public static void main(String[] args) {
-
             // Lanzar interfaz again
+                launch(args);
+        }
+
+        @Override
+        public void start(Stage primaryStage) throws Exception{
+                primaryStage.setTitle("DialTest");
+
+                button = new Button();
+                button.setText("Click me");
+
+                StackPane layout = new StackPane();
+                layout.getChildren().add(button);
+
+                Scene scene = new Scene(layout, 300, 250);
+                primaryStage.setScene(scene);
+                primaryStage.show();
         }
 }
 
