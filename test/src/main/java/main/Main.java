2947de79d6d72658fde659e77e0677cd30d89325
katty <katherine.simbana@dialapplet.com>
2019-04-30 16:19:52 +0200

    Poner if para evitar que pete al cerrar :)

--- test/src/main/java/main/Main.java
+++ test/src/main/java/main/Main.java
@@ -32,8 +32,10 @@ public class Main extends Application {
                 primaryStage.setScene(scene);
                 primaryStage.show();
                 primaryStage.setOnCloseRequest( event -> {
-                        event.consume();
-                        controller.totalClose();
+                        if (controller != null) {
+                                event.consume();
+                                controller.totalClose();
+                        }
                 });
         }
 
